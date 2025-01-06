555a8d2dac8df45893cdf22d0b83e3231032a638
Shedrack Okunda <okundashedrack@gmail.com>
2025-01-06 16:11:02 +0300

    Created a protected component for the protected routes

---
+++ src/features/auth/components/Protected.jsx
@@ -0,0 +1,13 @@
+import { useSelector } from "react-redux";
+import { SelectLoggedInUser } from "../AuthSlice";
+import { Navigate } from "react-router";
+
+export const Protected = ({ children }) => {
+  const loggedInUser = useSelector(SelectLoggedInUser);
+
+  if (loggedInUser?.isVerified) {
+    return children;
+  }
+
+  return <Navigate to={"/login"} />;
+};
